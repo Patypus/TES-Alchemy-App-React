b550df1dd08bb7e96de18e1a3f0f69068d30aa51
patrick howell <patrick.howell@outlook.com>
2021-08-19 11:15:35 +0100

    Added vector icons library, included it in the build and updated the home button to use a home icon

--- components/HomeButton.js
+++ components/HomeButton.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Pressable, Text } from 'react-native';
 import { general } from '../branding/styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export const HomeButton = ({navigation}) => {
     return (<Pressable
         onPress={() => navigation.navigate('Home')}
     >
-        <Text style={general.text}>Home</Text>
+        <Icon name="home" size={30} color="#FFF" />
     </Pressable>)
 }
